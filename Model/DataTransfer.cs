35f29da7b3e141c561a9453df8a6ab19284563ba
Jogger <beltchikov@gmx.de>
2021-07-10 20:30:12 +0200

    Issue "The conversion of a datetime2..." partialy solved

--- Model/DataTransfer.cs
+++ Model/DataTransfer.cs
@@ -10,6 +10,6 @@ namespace FmpDataContext.Model
         [Key]
         public Guid Id { get; set; }
         public DateTime Start { get; set; }
-        public DateTime End { get; set; }
+        public DateTime? End { get; set; }
     }
 }
